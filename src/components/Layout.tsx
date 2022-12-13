d8dfcb30c63247193a3e4ba7ce48c86f5323a58c
Kyle Esterhuizen <kyle.esterhuizen@directtransact.co.za>
2022-12-13 11:08:24 +0200

    added layout header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,8 +18,15 @@ function Layout({ children, bgColor }: Props) {
         <meta name="description" content="3D Secure POC" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>Please open your browser&apos;s devtools and inspect the network tab to view the the flow of data</header>
       <div className="container">{children}</div>
       <style jsx>{`
+        header {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 30px;
+        }
         .container {
           background-color: ${bgColor};
           min-height: 100vh;
